0dc8010099a00c1895ad461548c32d0653bfa139
jessebadry <45022943+jessebadry@users.noreply.github.com>
2021-07-04 20:54:31 -0700

    Added setup file for heroku

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+   name='DM-Manager',
+   version='1.0',
+   packages=['DM-Manager'],  #same as name
+   install_requires=['discord.py'], #external packages as dependencies
+)
